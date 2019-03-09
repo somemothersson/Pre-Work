044f37edec03c213c6fae71e5f7b80c174bec812
Stephen Malito <stevemalito@gmail.com>
2019-03-08 23:21:51 -0600

    module 10 done

---
+++ Module-10/javascript.js
@@ -0,0 +1,24 @@
+
+document.getElementById("growBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("bluekBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+document.getElementById("fadeBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity =".5";
+
+});
+
+document.getElementById("resetBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
